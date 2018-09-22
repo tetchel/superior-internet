d26acaebd535a10bdd1e489095179d0602975cc4
Architectonic <architectonic@localhost.localdomain>
2018-09-22 17:06:08 -0400

    adds triangle component

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import InferData from './infer-data'
 import Svg from './components/svg'
 import Template from './components/template'
+import Triangles from './components/triangular'
 
 // printToScreen(JSON.stringify(InferData(), null, 2))
 
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <div>
         <Svg></Svg>
+        <Triangles/>
         <Template name="template component" />
         <pre>
           {JSON.stringify(InferData(), null, 2)}
