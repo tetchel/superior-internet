c6359e91396a51e873fd656f169402ce8c62572c
Chris Marlow <christopher.marlow@shopify.com>
2018-09-22 14:47:39 -0400

    Got a component rendering!

--- client/app.js
+++ client/app.js
@@ -1,9 +1,18 @@
+import React, { Component } from 'react'
 import InferData from './infer-data'
 
-const printToScreen = (text) => {
-  const el = document.createElement('pre')
-  el.textContent = text
-  document.body.append(el)
+// printToScreen(JSON.stringify(InferData(), null, 2))
+
+class App extends Component {
+  constructor() {
+    super()
+    console.log('app loaded')
+    console.log(InferData())
+  }
+
+  render() {
+    return <pre> {JSON.stringify(InferData(), null, 2)} </pre>
+  }
 }
 
-printToScreen(JSON.stringify(InferData(), null, 2))
+export default App
