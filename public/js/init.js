a70351ecfef6d15db6dd9d2ace73cc30ab867291
Tim Etchells <tetchel@gmail.com>
2018-09-22 13:06:45 -0400

    change start command

--- public/js/init.js
+++
@@ -1,9 +0,0 @@
-info = info || "Couldn't get info";
-
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-  }
-
-console.log("My seed is " + getCookie("id"));
