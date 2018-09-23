351d01dcde2680b5be836abee6c93b78a5eedc31
Architectonic <architectonic@localhost.localdomain>
2018-09-23 16:00:16 -0400

    added dynamic background colours

--- client/components/backgroundcolour.js
+++ client/components/backgroundcolour.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { normalize } from '../normalize'
+import { digest } from 'json-hash'
 
 class Background extends Component {
   constructor(props) {
-    super(props)
-    console.log(data);
+    super(props);
+    data = digest(data);
+
     var r = normalize(data[0]);
     var g = normalize(data[1]);
     var b = normalize(data[2]);
-    console.log("background",r,g,b);
+
 
     document.querySelector('body').style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
